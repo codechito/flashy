2590cc0037afd9493a4820d917f1b61284d06188
Chito Cascante <chito@burstsms.com>
2019-08-03 06:07:54 +0800

    fix package

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -155,7 +155,7 @@ axios(options)
         campaigns: campaigns,
         campaign_list: campaign_list,
         contents: {}, 
-        idx: 0,
+        idx: -1,
         tester: '',
         element_type: [
           { value: 'Text', text: 'Text' },
