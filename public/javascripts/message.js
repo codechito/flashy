20fd16fb4e9e2944ef4da5d905402471c660cb15
Chito Cascante <chito@burstsms.com>
2019-08-04 14:19:46 +0800

    fix js

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -125,14 +125,14 @@ Vue.component('element-carousel', {
                 <b-form-textarea v-model="image.description" size="sm" class="form-control"></b-form-textarea>
               </b-form-group>
               <b-button v-on:click="addCardSuggestion(imgkey)" variant="outline-info" size="sm" href="#">New Card Suggestion</b-button>
-              <suggestion v-for="(suggestion, ckey) in image.card_suggestions" 
+              <suggestion v-for="(sgstn, ckey) in image.card_suggestions" 
                 v-bind:csidx="ckey" 
                 v-bind:element="element" 
                 v-bind:contents="contents" 
                 v-bind:idx="idx" 
                 v-bind:sidx="sidx" 
                 v-bind:imgidx="imgkey" 
-                v-bind:suggestion="suggestion" 
+                v-bind:suggestion="sgstn" 
                 v-bind:suggestion_type="suggestion_type"
               ></suggestion>
             </b-card-text>
