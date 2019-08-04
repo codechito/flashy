de3cc28c5741b3772dc25e7a599220651cc1c3df
Chito Cascante <chito@burstsms.com>
2019-08-04 12:26:15 +0800

    fix js

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -25,7 +25,7 @@ Vue.component('suggestion', {
           this.sidx = 0;
         }
         if(this.imgidx >= 0){
-          console.log(app.contents.messages[this.idx],app.contents.messages[this.idx].elements[this.sidx],app.contents.messages[this.idx].elements[this.sidx].images[imgidx]);
+          console.log("|",app.contents.messages[this.idx],"|",app.contents.messages[this.idx].elements[this.sidx],"|",app.contents.messages[this.idx].elements[this.sidx].images[imgidx]);
           this.contents.messages[this.idx].elements[this.sidx].images[this.imgidx].card_suggestions.splice(this.csidx, 1); 
         }
         else{
@@ -115,7 +115,7 @@ Vue.component('element-carousel', {
       if(!this.sidx){
         this.sidx = 0;
       }
-      console.log(app.contents.messages[this.idx],app.contents.messages[this.idx].elements[this.sidx],app.contents.messages[this.idx].elements[this.sidx].images[imgidx]);
+      console.log("|",app.contents.messages[this.idx],"|",app.contents.messages[this.idx].elements[this.sidx],"|",app.contents.messages[this.idx].elements[this.sidx].images[imgidx]);
       app.contents.messages[this.idx].elements[this.sidx].images[imgidx].card_suggestions.push({
         type: 'Link URL'
       });
