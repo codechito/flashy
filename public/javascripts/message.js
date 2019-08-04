a88c036e35b73d124cab1e6e2764b7d12a372af3
Chito Cascante <chito@burstsms.com>
2019-08-04 13:48:27 +0800

    fix js

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -33,7 +33,7 @@ Vue.component('suggestion', {
   template: `
   <div class="suggestion">
     <span>{{suggestion.type}}</span>
-    <b-button v-on:click="removeSuggestion()" v-b-tooltip.hover title="Remove suggestion" variant="info" class="icon-button float-right" ><h3> &times; </h3></b-button>
+    <b-button v-on:click="removeSuggestion(imgidx)" v-b-tooltip.hover title="Remove suggestion" variant="info" class="icon-button float-right" ><h3> &times; </h3></b-button>
     <hr/><br/>
     <b-form-group description="Suggestion Type" label-size="sm">
       <b-form-select :options="suggestion_type" size="sm" class="form-control" v-model="suggestion.type"></b-form-select>
